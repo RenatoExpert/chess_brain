69c6bd1cb4d19af19b980758164d5900827b59bd
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-02-20 19:22:08 -0300

    convert index to cartesian and cartesian to human

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 fn main() {
-    one_two(7);
-    one_two(63);
-    one_two(0);
-    one_two(10);
+    let test = index_cartesian(10);
+	cartesian_human(test);
 }
 
-fn one_two(index: u8) -> (u8, u8) {
+fn index_cartesian(index: u8) -> (u8, u8) {
 	let cartesian: (u8, u8) = (index % 8, index / 8);
-	println!("convert 1D {} to cartesian {:?}", index, cartesian);
+	println!("convert index {} to cartesian {:?}", index, cartesian);
 	return cartesian;
+}
+
+fn cartesian_human(cartesian: (u8,u8)) -> String {
+	let human: String = format!("{}{}", char::from_u32(cartesian.0 as u32 + 65).unwrap(), (cartesian.1 + 1));
+	println!("convert cartesian {:?} to human {}", cartesian, human);
+	return human;
 }
